7a26ef65e92dffe7aa8ef49d52baf2e38109b76b
Arunvel Sriram <arunvelsriram@gmail.com>
2019-10-02 19:26:52 +0530

    Release project through bintray

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,9 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import java.util.Date
 
 plugins {
     kotlin("jvm") version "1.3.50"
+    id("com.jfrog.bintray") version "1.8.4"
 }
 
 group = "dev.arunvelsriram.ktlint.rulesets.junit"
@@ -15,6 +17,7 @@ repositories {
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
     implementation("com.pinterest.ktlint:ktlint-core:0.34.2")
+    compileClasspath("com.jfrog.bintray.gradle:gradle-bintray-plugin:1.8.4")
 
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.5.2")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.5.2")
@@ -30,4 +33,30 @@ tasks.withType<KotlinCompile> {
 
 val test by tasks.getting(Test::class) {
     useJUnitPlatform { }
+}
+
+val currentVersion = project.version.toString()
+bintray {
+    user = "arunvelsriram"
+    key =  System.getenv("BINTRAY_API_KEY")
+    setConfigurations("archives")
+    with(pkg) {
+        repo = "maven"
+        name = "ktlint-ruleset-junit"
+        description = "Custom ktlint ruleset for linting JUnit tests."
+        setLabels("kotlin", "ktlint", "junit")
+        publish = true
+        override = true
+        publicDownloadNumbers = true
+        setLicenses("Unlicense")
+        vcsUrl = "https://github.com/arunvelsriram/ktlint-ruleset-junit.git"
+        websiteUrl = "https://github.com/arunvelsriram/ktlint-ruleset-junit"
+        issueTrackerUrl = "https://github.com/arunvelsriram/ktlint-ruleset-junit/issues"
+        githubRepo = "arunvelsriram/ktlint-ruleset-junit"
+        with(version) {
+            name = currentVersion
+            vcsTag = "v$currentVersion"
+            released = Date().toString()
+        }
+    }
 }
